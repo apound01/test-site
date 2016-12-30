75244a689955197a724cce7e23c685a48a2a3920
Amy <amy.pound@icloud.com>
2016-12-30 01:01:26 +0000

    index

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.port || 1337
 
 app.get('/', function (req, res) {
-    res.render("admin_7/index");
+    res.render("index");
 });
 
 var server = app.listen(port, function () {
