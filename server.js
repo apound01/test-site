acee3639d36c30d87ef8fea219bfce02b7f7d64e
Amy <amy.pound@icloud.com>
2016-12-30 02:03:30 +0000

    error fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.port || 1337
 
 app.get('/', function (req, res) {
-    res.render("index.html);
+    res.render("index.html");
 });
 
 var server = app.listen(port, function () {
