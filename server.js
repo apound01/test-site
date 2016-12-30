276c9e58594defb4cf9d8609141a27387c4c4729
Amy <amy.pound@icloud.com>
2016-12-30 01:41:55 +0000

    view engine

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 ﻿var express = require('express');
+var bodyParser = require("body-parser");
+var engines = require('consolidate');
 var app = express();
 var port = process.env.port || 1337
 
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
 app.get('/', function (req, res) {
-    res.render("index.html");
+    res.render("index");
 });
 
 var server = app.listen(port, function () {
